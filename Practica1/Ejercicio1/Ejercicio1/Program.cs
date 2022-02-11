6f9d004ac2cd2e26b98891bcaab3ced2e3d5a012
David300902 <vad0029612@est.univalle.edu>
2022-02-11 11:24:58 -0400

    ------.

---
+++ Practica1/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("Cuantos datos desea ingresar?");
+            int num = int.Parse(Console.ReadLine());
+            Persona[,] registro = new Persona[num, num];
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.WriteLine("Ingrese el nombre");
+                    name = Console.ReadLine();
+                    Console.WriteLine("Ingrese la edad");
+                    age = int.Parse(Console.ReadLine());
+
+                }
+            }
+
+        }
+    }
+
+    class Persona
+    {
+        string nombre;
+        int edad;
+    }
+}
---
+++ Practica1/Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[10];
+            Random r = new Random();
+
+            Console.WriteLine("el array es: ");
+            llenado(10);
+
+            Console.WriteLine();
+            Console.WriteLine("El resultado con el metodo es:");
+
+            metodo(10);
+
+            Console.ReadKey();
+
+
+            
+            void llenado(int num)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    numeros[i] = r.Next(1, 10);
+                    Console.Write(numeros[i] + " ");
+                }
+            }
+
+            void metodo(int num)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    for (int j = i+1; j < num; j++)
+                    {
+                        if (numeros[j] < numeros[i])
+                        {
+                            int aux = numeros[i];
+                            numeros[i] = numeros[j];
+                            numeros[j] = aux;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write(numeros[i] + " ");
+                }
+
+            }
+        }
+
+        
+    }
+
+ 
+}
